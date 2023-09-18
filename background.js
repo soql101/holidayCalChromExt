a11267ae102e3a80014f80a796530fa373943fda
Ghanshyam <gc.chou17@gmail.com>
2023-09-18 10:23:01 +0530

    call after every hour

--- background.js
+++ background.js
@@ -2,33 +2,35 @@
 chrome.action.setBadgeText({ text: '' });
 
 // Function to update the badge text once a day at the start of the day
-function updateBadgeOnceADay() {
-    const currentDate = new Date();
-    const tomorrow = new Date(currentDate);
-    tomorrow.setDate(currentDate.getDate() + 1);
-    tomorrow.setHours(0, 0, 0, 0); // Set the time to midnight of the next day
+function updateBadgeEveryHour() {
+    const oneHour = 60 * 60 * 1000; // One hour in milliseconds
 
-    const timeUntilMidnight = tomorrow - currentDate;
+    // Call the updateBadge function immediately when this is first called
+    updateBadge();
 
-    setTimeout(() => {
-        chrome.storage.local.get('selectedCustomDate', (data) => {
-            const selectedCustomDate = data.selectedCustomDate;
-            if (selectedCustomDate) {
-                const holidayDate = new Date(selectedCustomDate + 'T00:00:00'); // Set time to midnight
-                const timeRemaining = holidayDate - currentDate;
-                const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
-                const badgeText = `${days+1} days`;
-                chrome.action.setBadgeText({ text: badgeText });
-            }
-        });
-
-        // Schedule the next update at the start of the next day
-        updateBadgeOnceADay();
-    }, timeUntilMidnight);
+    // Schedule the next update every one hour
+    setInterval(() => {
+        updateBadge();
+    }, oneHour);
 }
 
-// Call the function to start updating the badge text once a day
-updateBadgeOnceADay();
+function updateBadge() {
+    const currentDate = new Date();
+
+    chrome.storage.local.get('selectedCustomDate', (data) => {
+        const selectedCustomDate = data.selectedCustomDate;
+        if (selectedCustomDate) {
+            const holidayDate = new Date(selectedCustomDate + 'T00:00:00'); // Set time to midnight
+            const timeRemaining = holidayDate - currentDate;
+            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+            const badgeText = `${days + 1} days`;
+            chrome.action.setBadgeText({ text: badgeText });
+        }
+    });
+}
+
+// Call the function to start updating the badge every hour
+updateBadgeEveryHour();
 
 // Listen for messages from the popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
